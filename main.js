94cf26ea6c6cfda51c7772bbe9a08f4851253558
Simon Frezard <simon.frezard@gmail.com>
2020-12-15 14:37:55 +0100

    ajout propriete parent & enfant + cycle de vie

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const RootComponent = {
     data() {
         return {
-            title: "Hello my Vue v3"
+            title: "Hello my Vue v3",
+            property: "Are you OK Annie ?"
         }
     },
     methods: {
-
+        wakeUp() {
+            alert("wow");
+        }
+    },
+    created() {
+        console.log("CREATED !!!");
+    },
+    mounted() {
+        console.log("MOUNTED .. !");
     }
 };
+
 const app = Vue.createApp(RootComponent);
--- mycomponent.js
+++ mycomponent.js
@@ -1,7 +1,15 @@
 /* attention le nom du composant est TRES important, il va être utilisé
 * sous forme de balise (comme React)*/
 app.component("mycomponent", {
-    template:'<p><button @click="addCount()">CLICK ME PLEASE {{ count }}</button></p>',
+    template:`<p><button @click="addCount() ; sendToParent()">CLICK ME PLEASE {{ count }}</button></p>
+        <h3>{{ myprops }}</h3>`,
+    props: {
+        myprops: {
+            type: String,
+            /*les differents types: String, Array, Number,
+            * Array et Boolean*/
+        }
+    },
     data(){
         return {
             count: 0,
@@ -10,6 +18,10 @@ app.component("mycomponent", {
     methods: {
         addCount() {
             this.count++;
+        },
+        sendToParent() {
+            this.$emit("hello-parent");
+            /*pour pouvoir émettre un événement*/
         }
 
     },
