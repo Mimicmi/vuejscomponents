117c10a77f0686d26092558e76845d06a7ad0f17
Simon Frezard <simon.frezard@gmail.com>
2020-12-15 12:21:08 +0100

    bouton fonctionnel incrementation

--- mycomponent.js
+++ mycomponent.js
@@ -1,13 +1,16 @@
 /* attention le nom du composant est TRES important, il va être utilisé
 * sous forme de balise (comme React)*/
 app.component("mycomponent", {
-    template:'<button>CLICK ME PLEASE</button>',
+    template:'<button @click="addCount()">CLICK ME PLEASE {{ count }}</button>',
     data(){
         return {
             count: 0,
         }
     },
     methods: {
+        addCount() {
+            this.count++;
+        }
 
     },
     computed: {
